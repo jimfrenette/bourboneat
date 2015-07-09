22495e14365ec7e26123fe2c3c5fc6ef6091dcc0
jimfrenette <jimfrenette@yahoo.com>
2015-07-08 21:05:46 -0400

    rm extra title tag

--- header.php
+++ header.php
@@ -12,7 +12,6 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-	<title><?php wp_title( ' | ', true, 'right' ); ?></title>
 	<?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
