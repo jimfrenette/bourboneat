c14f50aebdca3ca7412b98431c864f4f04f6dfaa
jimfrenette <jimfrenette@yahoo.com>
2015-09-07 18:41:48 -0400

    escape time_entry

--- functions/entry.php
+++ functions/entry.php
@@ -51,10 +51,18 @@ function entry_meta() {
 	}
 
 	if ( in_array( get_post_type(), array( 'post', 'attachment' ) ) ) {
-		$time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
+
+		$allowed_time_html = array(
+		    'time' => array(
+		        'class' => array(),
+		        'datetime' => array()
+		    )
+		);
+
+		$time_string = wp_kses( '<time class="entry-date published updated" datetime="%1$s">%2$s</time>', $allowed_time_html );
 
 		if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
-			$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s">%4$s</time>';
+			$time_string = wp_kses( '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s">%4$s</time>', $allowed_time_html );
 		}
 
 		$time_string = sprintf( $time_string,
